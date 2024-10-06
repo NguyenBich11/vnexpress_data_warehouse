f2e05e3f70fbfe05975f46ba47c39238a496f2dc
Nguyễn Thị Ngọc Bích <127025193+NguyenBich11@users.noreply.github.com>
2024-10-06 21:25:12 +0700

    Update settings.py class new

--- vnexpress_data_warehouse/settings.py
+++ vnexpress_data_warehouse/settings.py
@@ -7,6 +7,8 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+#  for Chrome driver 
+
 BOT_NAME = "vnexpress_data_warehouse"
 
 SPIDER_MODULES = ["vnexpress_data_warehouse.spiders"]
@@ -25,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -62,9 +64,12 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "vnexpress_data_warehouse.pipelines.VnexpressDataWarehousePipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "vnexpress_data_warehouse.pipelines.CSVDBVnexpressDataWarehousePipeline": 100,
+   "vnexpress_data_warehouse.pipelines.JsonDBVnexpressDataWarehousePipeline": 200,
+   "vnexpress_data_warehouse.pipelines.MongoDBVnexpressDataWarehousePipeline": 400,
+   "vnexpress_data_warehouse.pipelines.ProcessDataToPostgreSQLPipeline": 500,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
